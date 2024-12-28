ad7a8d15b0a874048e3afcc55f7ce3e3546a47fa
Willyan Gabriel <145140640+Willyang10x@users.noreply.github.com>
2024-12-28 14:52:06 -0300

    Chamada do header com js

--- Components/Home/site.js
+++ Components/Home/site.js
@@ -1,4 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
+  if (!document.querySelector('header')) {
+    fetch('/Components/Header/header.html')  
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro ao carregar o header: ' + response.status);
+        }
+        return response.text();
+      })
+      .then(data => {
+        
+        const headerContainer = document.createElement('header');
+        headerContainer.innerHTML = data;
+
+        
+        document.getElementById('header-placeholder').appendChild(headerContainer);
+      })
+      .catch(error => console.error(error));
+  }
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
   if (!document.querySelector('footer')) {
     fetch('/Components/Footer/footer.html')
       .then(response => {
