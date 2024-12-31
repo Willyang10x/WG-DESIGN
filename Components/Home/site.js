3152156ca5e113bdfa289257825c8077de4e5199
Willyan Gabriel <145140640+Willyang10x@users.noreply.github.com>
2024-12-30 23:17:51 -0300

    Atualizações interuptor, e ajustes no site

--- Components/Home/site.js
+++ Components/Home/site.js
@@ -42,32 +42,61 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 const modeToggleBtn = document.getElementById("modeToggleBtn");
+const modeIcon = document.getElementById("modeIcon");
 const body = document.body;
 
+// Caminhos das imagens
+const lightModeIcon = "/Assets/icons/interruptor1.png";
+const darkModeIcon = "/Assets/icons/interruptor2.png";
+
 modeToggleBtn.addEventListener("click", () => {
+    // Alterna o modo escuro
     body.classList.toggle("dark-mode");
+    
+    // Troca o ícone
+    if (body.classList.contains("dark-mode")) {
+        modeIcon.src = darkModeIcon; // Ícone para modo escuro
+    } else {
+        modeIcon.src = lightModeIcon; // Ícone para modo claro
+    }
 });
 
 
 function openModal(imgElement) {
-    var modal = document.getElementById("myModal");
-    var modalImg = document.getElementById("img01");
-    modal.style.display = "block";
-    modalImg.src = imgElement.src;
-  }
-  
- 
-  function closeModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "none";
-  }
-  
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("img01");
+  modal.style.display = "block";
+  modalImg.src = imgElement.src;
+}
 
-  window.onclick = function(event) {
-    var modal = document.getElementById("myModal");
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  var modal = document.getElementById("myModal");
+  if (event.target === modal) {
+    closeModal();
   }
+};
+
+
+window.onscroll = function() {
+  const backToTopButton = document.getElementById("backToTop");
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    backToTopButton.style.display = "flex";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+};
+
+// Voltar suavemente ao topo
+document.getElementById("backToTop").onclick = function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+};
   
   
